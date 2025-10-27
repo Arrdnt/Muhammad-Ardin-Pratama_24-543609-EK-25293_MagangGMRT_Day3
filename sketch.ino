1c155a60057b67e6e8efaedc226600637f1bb5d1
Ar not R <153404908+Arrdnt@users.noreply.github.com>
2025-10-27 21:18:55 +0700

    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -5,7 +5,7 @@
 
 Servo myServos[5];
 Adafruit_MPU6050 mpu;
-int servoPins[5] = {12, 27, 14, 26, 13};
+int servoPins[5] = {12, 14, 27, 26, 13};
 float rollpitchsense = 0.5;
 float yawsense = 0.5;
 
@@ -16,43 +16,61 @@ void setup() {
         myServos[i].attach(servoPins[i]);
         myServos[i].write(90);
     }
+
+    while (!mpu.begin()) {
+      Serial.println("Failed");
+      delay(500);
+    }
     
     pinMode(18, INPUT);
 }
 
 void loop() {
-    int gerakanManusia = digitalRead(18);
-    while (!mpu.begin()) {
-    Serial.println("Failed to find a valid MPU6050 sensor, please check the wiring!");
-    delay(500);
+  int gerakanManusia = digitalRead(18);
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+  if (gerakanManusia == HIGH) {
+    for (int i = 0; i < 5; i++) {
+      myServos[i].write(45);
     }
+    delay(1000);
 
-    sensors_event_t a, g, temp;
-    mpu.getEvent(&a, &g, &temp);
-    if(gerakanManusia == HIGH) {
-        
-        for(int i = 0; i < 5; i++) {
-            myServos[i].write(45);
-        }
-        delay(1000);
-        
-        for(int i = 0; i < 5; i++) {
-            myServos[i].write(90);
-        }
-        delay(3000);
+    for (int i = 0; i < 5; i++) {
+      myServos[i].write(90);
     }
-    else if (gerakanManusia != HIGH) {
-      float roll = a.acceleration.x;
-      float pitch = a.acceleration.y;
-      float yaw = g.gyro.z;
+    delay(3000);
+  }
+  else if (gerakanManusia != HIGH) {
+    float roll = a.acceleration.x;
+    float pitch = a.acceleration.y;
+    float yaw = g.gyro.z;
 
-      if (roll > rollpitchsense) {
-        myServos[0].write(0);
-        myServos[1].write(0);
-      }else if(roll < rollpitchsense) {
-        myServos[0].write(180);
-        myServos[1].write(180); 
-      }
+    if (roll > rollpitchsense) {
+      myServos[0].write(0);
+      myServos[1].write(0);
+    } 
+    else if (roll < rollpitchsense) {
+      myServos[0].write(180);
+      myServos[1].write(180);
     }
-    delay(100);
+    if (pitch > rollpitchsense) {
+      myServos[2].write(180);
+      myServos[3].write(180);
+    } 
+    else if (pitch < rollpitchsense) {
+      myServos[2].write(0);
+      myServos[3].write(0);
+    }
+    if (yaw > yawsense) {
+      myServos[4].write(180);
+      delay(1000);
+      myServos[4].write(90);
+    } 
+    else if (yaw < yawsense) {
+      myServos[4].write(0);
+      delay(1000);
+      myServos[4].write(90);
+    }
+    delay(2000);
+  }
 }
